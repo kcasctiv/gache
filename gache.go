78eeac8185ab45b98bc8d49cd6343443e07b0056
Mikhail Alyogin <kcasctiv@hotmail.com>
2019-02-13 20:47:54 +0400

    Add cache implementation

--- gache.go
+++ gache.go
@@ -1,6 +1,7 @@
 package gache
 
 import (
+	"fmt"
 	"sync"
 	"time"
 )
@@ -11,7 +12,7 @@ type Cache interface {
 	NewGroup(key string, expiration time.Duration, fillFunc FillFunc) error
 	DelGroup(key string)
 	GetGroupVal(gkey, vkey string) (interface{}, bool)
-	SetGroupVal(gkey, vkey string, val interface{})
+	SetGroupVal(gkey, vkey string, val interface{}) error
 }
 
 type Group interface {
@@ -27,6 +28,82 @@ type FillFunc func(key string) (interface{}, bool)
 type cache struct {
 	*group
 	groups map[string]*group
+}
+
+func NewCache(expiration time.Duration, fillFunc FillFunc) Cache {
+	if expiration < 0 {
+		expiration = 0
+	}
+
+	return &cache{
+		group: &group{
+			values:     make(map[string]value),
+			fillFunc:   fillFunc,
+			expiration: expiration,
+		},
+		groups: make(map[string]*group),
+	}
+}
+
+func (c *cache) Group(key string) (Group, bool) {
+	c.mx.Lock()
+	g, ok := c.groups[key]
+	c.mx.Unlock()
+
+	return g, ok
+}
+
+func (c *cache) NewGroup(key string, expiration time.Duration, fillFunc FillFunc) error {
+	c.mx.Lock()
+	defer c.mx.Unlock()
+
+	if _, exists := c.groups[key]; exists {
+		return fmt.Errorf("group with key %q already exists", key)
+	}
+
+	if expiration < 0 {
+		expiration = 0
+	}
+
+	c.groups[key] = &group{
+		values:     make(map[string]value),
+		fillFunc:   fillFunc,
+		expiration: expiration,
+	}
+
+	return nil
+}
+
+func (c *cache) DelGroup(key string) {
+	c.mx.Lock()
+	delete(c.groups, key)
+	c.mx.Unlock()
+}
+
+func (c *cache) GetGroupVal(gkey, vkey string) (interface{}, bool) {
+	c.mx.Lock()
+	g, ok := c.groups[gkey]
+	c.mx.Unlock()
+
+	if !ok {
+		return nil, false
+	}
+
+	return g.Get(vkey)
+}
+
+func (c *cache) SetGroupVal(gkey, vkey string, val interface{}) error {
+	c.mx.Lock()
+	g, ok := c.groups[gkey]
+	c.mx.Unlock()
+
+	if !ok {
+		return fmt.Errorf("group with key %q doesn't exist", gkey)
+	}
+
+	g.Set(vkey, val)
+
+	return nil
 }
 
 type value struct {
@@ -59,6 +136,7 @@ func (g *group) Get(key string) (interface{}, bool) {
 	}
 
 	v.data, ok = g.fillFunc(key)
+
 	if !ok {
 		g.mx.Lock()
 		delete(g.values, key)
@@ -66,7 +144,10 @@ func (g *group) Get(key string) (interface{}, bool) {
 		return nil, false
 	}
 
-	v.expiration = now.Add(g.expiration).UnixNano()
+	if g.expiration != 0 {
+		v.expiration = now.Add(g.expiration).UnixNano()
+	}
+
 	g.mx.Lock()
 	g.values[key] = v
 	g.mx.Unlock()
@@ -76,10 +157,17 @@ func (g *group) Get(key string) (interface{}, bool) {
 
 func (g *group) Set(key string, val interface{}) {
 	g.mx.Lock()
+
+	var expiration int64
+	if g.expiration != 0 {
+		expiration = time.Now().Add(g.expiration).UnixNano()
+	}
+
 	g.values[key] = value{
 		data:       val,
-		expiration: time.Now().Add(g.expiration).UnixNano(),
+		expiration: expiration,
 	}
+
 	g.mx.Unlock()
 }
 
